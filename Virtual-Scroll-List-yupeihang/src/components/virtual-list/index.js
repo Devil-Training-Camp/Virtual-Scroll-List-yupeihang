d59410405897cdcbda89d594a98a0220099fea08
srd18505604506 <yupeihang-xf@bestpay.com.cn>
2023-08-29 18:03:55 +0800

    npm配置

---
+++ Virtual-Scroll-List-yupeihang/src/components/virtual-list/index.js
@@ -0,0 +1,5 @@
+import VirtualList from './virtual-list';
+
+VirtualList.install = Vue => Vue.component(VirtualList.name, VirtualList);
+
+export default VirtualList;
---
+++ Virtual-Scroll-List-yupeihang/src/index.js
@@ -0,0 +1,21 @@
+import VirtualList from './components/virtual-list/index';
+
+const components = [
+  VirtualList
+]
+
+const install = function(Vue, opts = {}) {
+  components.map(component => {
+    Vue.component(component.name, component);
+  })
+}
+
+/* 支持使用标签的方式引入 */
+if (typeof window !== 'undefined' && window.Vue) {
+  install(window.Vue);
+}
+
+export default {
+  install,
+  VirtualList
+}
